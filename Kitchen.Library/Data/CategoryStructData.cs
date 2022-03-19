7c5725ded2c3a999cbbfed740846eef015e5ad9e
Dawid Król <dawidkrol7@gmail.com>
2022-03-19 10:16:24 +0100

    Parallel downloading of categories with a lock

--- Kitchen.Library/Data/CategoryStructData.cs
+++ Kitchen.Library/Data/CategoryStructData.cs
@@ -1,5 +1,6 @@
 ﻿using Kitchen.Library.DataModels;
 using Kitchen.Library.DbAccess;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +12,32 @@ namespace Kitchen.Library.Data
     public class CategoryStructData : ICategoryStructData
     {
         private readonly ISqlDataAccess _data;
+        private readonly ILogger<CategoryStructData> _logger;
 
-        public CategoryStructData(ISqlDataAccess data)
+        public CategoryStructData(ISqlDataAccess data, ILogger<CategoryStructData> logger)
         {
             _data = data;
+            _logger = logger;
         }
         public async Task<IEnumerable<KategoriaData>> GetCategories()
         {
             var a =  await _data.LoadDataAsyncViews<KategoriaData>("select * from [dbo].[vwKategorie_Get]");
-            //Parallel.ForEach(a,async x => x.SubDirectories = (await GetSubdirectoriesById(x.Id)).ToList());
-            foreach (var x in a)
-            {
-                x.SubDirectories = (await GetSubdirectoriesById(x.Id)).ToList();
-            }
+
+            //Faster way
+            Parallel.ForEach(a, x =>
+                {
+                    lock (x)
+                    {
+                        x.SubDirectories = (GetSubdirectoriesById(x.Id).GetAwaiter().GetResult()).ToList();
+                    }
+                }
+            );
+
+
+            //foreach (var x in a)
+            //{
+            //    x.SubDirectories = (await GetSubdirectoriesById(x.Id)).ToList();
+            //}
             return a;
         }
         public async Task<IEnumerable<SubCategoriesData>> GetSubdirectoriesById(int id)
