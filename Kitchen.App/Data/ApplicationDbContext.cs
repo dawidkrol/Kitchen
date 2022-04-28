929298929eecd26282924dad58de98f3bf0db338
Dawid Król <dawidkrol7@gmail.com>
2022-04-28 08:22:18 +0200

    Updating IdentityUser type in Context class

--- Kitchen.App/Data/ApplicationDbContext.cs
+++ Kitchen.App/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Kitchen.App.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Kitchen.App.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUserModel>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
